16ca31a36a5374fda1caad7a2368071a5083719e
Zhenqing Hu <hustc12@gmail.com>
2025-01-30 10:07:37 -0500

    Commit a new script for building op_package. wq

---
+++ build_op_package.sh
@@ -0,0 +1,9 @@
+export QNN_SDK_ROOT=src/backends/qnn/qualcomm_ai_engine_direct_220/
+export ANDROID_NDK_ROOT=/media/huzq85/2TB_Software/software/softwareDev/AndroidSDK/ndk/26.3.11579264
+export PATH=$PATH:$ANDROID_NDK_ROOT
+
+source src/backends/qnn/HexagonSDK/setup_sdk_env.source
+source $QNN_SDK_ROOT/bin/envsetup.sh
+
+cd src/backends/qnn/LLaMAOpPackageHtp/LLaMAPackage/
+make htp_aarch64 && make htp_v73
