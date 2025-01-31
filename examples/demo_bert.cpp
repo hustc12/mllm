6c7a3460796220af9296f38eaeb722f09cc1e519
Zhenqing Hu <hustc12@gmail.com>
2025-01-31 10:26:48 -0500

    Minor update the bert demo.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

--- examples/demo_bert.cpp
+++ examples/demo_bert.cpp
@@ -29,11 +29,16 @@ int main(int argc, char *argv[]) {
     auto model = BertModel(config);
     model.load(model_path);
 
-    string text = "Help me set an alarm at 21:30";
-    vector<string> texts = {text, text};
+    string text1 = "Help me set an alarm at 21:30";
+    string text2 = "Large Language Models (LLMs) are advanced artificial intelligence systems designed to understand and generate human-like text. These models are trained on vast amounts of data, enabling them to perform a wide range of tasks, from answering questions and summarizing text to generating creative content and engaging in conversational dialogue. LLMs like GPT-3 and GPT-4, developed by OpenAI, have set new benchmarks in natural language processing by leveraging deep learning architectures, particularly transformer models, which excel at capturing context and relationships within text. The scalability and versatility of LLMs make them invaluable tools for applications in education, customer service, content creation, and more. However, their deployment also raises ethical considerations, including issues of bias, misinformation, and the potential for misuse. As the field continues to evolve, ongoing research and responsible deployment strategies are essential to harnessing the full potential of these powerful AI systems while mitigating their risks.\"\nGenerate a title based on the above text.";
+    vector<string> texts = {text1, text2};
     for (auto &text : texts) {
         auto inputs = tokenizer.tokenizes(text);
+        std::cout << "DEBUGGING - Input Length = " << inputs.at(0).size() << std::endl;
+        auto start_time = mllm_time_us();
         auto res = model({inputs[0], inputs[1], inputs[2]})[0];
+        auto end_time = mllm_time_us();
+        std::cout << "DEBUGGING - Inference Time = " << (end_time - start_time) / 1000.0F << "ms" << std::endl;
         res.printData<float>();
     }
 
