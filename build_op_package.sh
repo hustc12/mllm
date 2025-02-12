652721804f196cd020bb6ec00b9803e9b14eb7a3
Zhenqing Hu <hustc12@gmail.com>
2025-02-12 15:43:08 -0500

    Minor update the bash script for building op package.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

--- build_op_package.sh
+++ build_op_package.sh
@@ -1,5 +1,5 @@
 export QNN_SDK_ROOT=src/backends/qnn/qualcomm_ai_engine_direct_220/
-export ANDROID_NDK_ROOT=/media/huzq85/2TB_Software/software/softwareDev/AndroidSDK/ndk/26.3.11579264
+export ANDROID_NDK_ROOT=/home/huzq85/software/softwareDev/AndroidSDK/ndk/26.3.11579264
 export PATH=$PATH:$ANDROID_NDK_ROOT
 
 source src/backends/qnn/HexagonSDK/setup_sdk_env.source
