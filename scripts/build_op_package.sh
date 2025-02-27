14cdf554878606f5cc7770ca034c696e637ed5b6
Zhenqing Hu <hustc12@gmail.com>
2025-02-27 17:11:48 -0500

    Updated the bash script since moved out the Hexagon SDK.
    Signed-off-by: Zhenqing Hu <hustc12@gmail.com>

--- scripts/build_op_package.sh
+++ scripts/build_op_package.sh
@@ -2,7 +2,7 @@ export QNN_SDK_ROOT=../src/backends/qnn/qualcomm_ai_engine_direct_220/
 export ANDROID_NDK_ROOT=/home/huzq85/software/softwareDev/AndroidSDK/ndk/26.3.11579264
 export PATH=$PATH:$ANDROID_NDK_ROOT
 
-source ../src/backends/qnn/HexagonSDK/setup_sdk_env.source
+source /home/huzq85/software/softwareDev/Qualcomm/HexagonSDK/setup_sdk_env.source
 source $QNN_SDK_ROOT/bin/envsetup.sh
 
 cd ../src/backends/qnn/LLaMAOpPackageHtp/LLaMAPackage/
